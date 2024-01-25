d4e085cc4d4090d5c6bb8c05367a281667c3159b
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-01-25 13:10:03 -0600

    Bump python-documentcloud, handle APIError for permission failure

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from documentcloud.addon import AddOn, SoftTimeOutAddOn
+""" Requires SoftTimeOutAddOn and APIError from python-documentcloud """
+from documentcloud.addon import SoftTimeOutAddOn
+from documentcloud.exceptions import APIError
+
+
 class BulkDelete(SoftTimeOutAddOn):
+    """ DocumentCloud Add-On to bulk delete documents"""
     def main(self):
+        """ Checks that the user confirmed 
+        and then deletes the documents that they are authorized to delete."""
         confirm = self.data.get("confirm")
         if confirm is not None and confirm is True:
             for document in self.get_documents():
-                document.delete()
-        else: 
+                try:
+                    document.delete()
+                except APIError: # If user does not have permissions to delete the document, skip.
+                    pass
+        else:
             self.set_message("You did not confirm, this Add-On did nothing.")
+
 
 if __name__ == "__main__":
     BulkDelete().main()
